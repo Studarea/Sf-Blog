90942f2bce179c13ccea11a5fa7d33f47c65c70b
Cyril <graf>
2020-11-23 16:54:32 +0100

    first commit

---
+++ migrations/Version20201123153736.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201123153736 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, image VARCHAR(255) NOT NULL, date_published DATE NOT NULL, date_created DATE NOT NULL, published TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, color INT NOT NULL, date_published DATE NOT NULL, date_created DATE NOT NULL, published TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE category');
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+
+
+// J'ai créé une base de données en modifiant la variable DATABASE_URL
+// du fichier .env.local (que j'ai créé en copiant le fichier .env)
+// puis en utilisant la ligne de commande doctrine:database:create
+
+// J'ai créé une classe Book qui est une entité car elle possède l'annotation
+// @ORM\Entity
+
+/**
+ * @ORM\Entity()
+ */
+class Article
+
+{
+
+// Dans article : id, title, content, image, date de publication, date de création, est publié ? (oui / non)
+
+    // J'ai créé autant de propriétés que de colonnes voulues dans la table
+    // et j'ai mappé les propriétés avec des annotations et la classe ORM (attention
+    // de ne pas oublier le use correspondant)
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $image;
+
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_published;
+
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_created;
+
+
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $published;
+
+
+}
+
+
+?>
